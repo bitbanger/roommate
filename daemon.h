11e52d1b344d23df0f6c66ec52651ae507b10154
Kyle Smith <kes2887@rit.edu>
2014-02-02 14:32:02 -0500

    Increase position constant for door's locked state

--- daemon.h
+++ daemon.h
@@ -3,7 +3,7 @@
 
 const int GPIO_SERVO = 0;
 const int SERVO_RANGE = 200;
-const int SERVO_LOCKED = 20; // "angle"
+const int SERVO_LOCKED = 22; // "angle"
 const int SERVO_UNLOCKED = 11; // "angle"
 
 #endif
