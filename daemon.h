0699772f010ecca33a2201d9faa0d24596fdcf7c
Kyle Smith <kes2887@rit.edu>
2014-02-04 20:53:54 -0500

    Changes lock position constants just because

--- daemon.h
+++ daemon.h
@@ -3,8 +3,8 @@
 
 const int GPIO_SERVO = 0;
 const int SERVO_RANGE = 200;
-const int SERVO_LOCKED = 22; // "angle"
-const int SERVO_UNLOCKED = 11; // "angle"
+const int SERVO_LOCKED = 21; // "angle"
+const int SERVO_UNLOCKED = 10; // "angle"
 
 const int GPIO_RED = 4;
 const int GPIO_GREEN = 5;
